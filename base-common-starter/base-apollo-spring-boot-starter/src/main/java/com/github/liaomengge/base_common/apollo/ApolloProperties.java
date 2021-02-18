d8dc5362b7fba135fdc9459e12cf0371a7f0e2b9
mengge liao <liaomengge@gmail.com>
2021-02-18 19:38:33 +0800

    [Chore]: 1. add spring framework issue new version use case

--- base-common-starter/base-apollo-spring-boot-starter/src/main/java/com/github/liaomengge/base_common/apollo/ApolloProperties.java
+++ base-common-starter/base-apollo-spring-boot-starter/src/main/java/com/github/liaomengge/base_common/apollo/ApolloProperties.java
@@ -21,7 +21,7 @@ public class ApolloProperties {
     private ConditionalProperties conditional = new ConditionalProperties();
 
     /**
-     * 针对@RefreshScope注解刷新指定的keys
+     * 针对{@link RefreshTypeEnum#SCOPE}类型下，@RefreshScope注解刷新指定的keys
      */
     @Data
     public static class RefreshScopeProperties {
@@ -29,9 +29,13 @@ public class ApolloProperties {
     }
 
     /**
-     * 只针对@Bean @ConditionalOnProperty注解动态刷新符合条件的bean
+     * 只针对包含{@link RefreshTypeEnum#PROPERTIES}类型下，@Bean @ConditionalOnProperty注解动态刷新符合条件的bean
      * 注：
-     * 1. 被调用的bean，不能被@Autowired(required = false)注解
+     * 1. 被调用的bean，不能被@Autowired(required = false)注解，
+     * 只能通过{@link org.springframework.context.ApplicationContext#getBean()获取}或者
+     * {@link org.springframework.beans.factory.ObjectProvider}注入
+     * 2. 只有在${spring-framework.version} >= 5.2.13.RELEASE或者${spring-framework.version} >=5.3.4.RELEASE，
+     * 见github issue: https://github.com/spring-projects/spring-framework/issues/26518
      */
     @Data
     public static class ConditionalProperties {
@@ -39,12 +43,18 @@ public class ApolloProperties {
         private List<ClassConditionalProperties> classConditionals = Lists.newArrayList();
     }
 
+    /**
+     * 指定扫描指定的类下面的所有@Bean @ConditionalOnProperty的对象
+     */
     @Data
     public static class ClassConditionalProperties {
         private String scanClassName;
         private List<BeanConditionalProperties> beanConditionals = Lists.newArrayList();
     }
 
+    /**
+     * 指定scan class下，匹配相同的methodName的beanName，默认：返回类型的SimpleName
+     */
     @Data
     public static class BeanConditionalProperties {
         private String methodName;
