ec9710750fb9df19384bdc6344f223c4287d3091
mengge liao <liaomengge@gmail.com>
2021-02-22 13:59:29 +0800

    [Chore]: 1. upgrade version

--- base-common-starter/base-tool-spring-boot-starter/src/main/java/com/github/liaomengge/base_common/consts/BaseConst.java
+++ base-common-starter/base-tool-spring-boot-starter/src/main/java/com/github/liaomengge/base_common/consts/BaseConst.java
@@ -6,5 +6,5 @@ package com.github.liaomengge.base_common.consts;
 public interface BaseConst {
 
     String BASE_FRAMEWORK_VERSION_NAME = "base-framework.version";
-    String BASE_FRAMEWORK_VERSION_VALUE = "3.0.0-SNAPSHOT";
+    String BASE_FRAMEWORK_VERSION_VALUE = "3.0.0.RELEASE";
 }
